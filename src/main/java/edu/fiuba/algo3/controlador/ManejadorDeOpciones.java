b949c34968fb9ba9afb4946f2df636b61dc2fa31
Micaela <micaelamjuarez@gmail.com>
2020-08-09 16:49:57 -0300

    Creo paquete controlador y ManejadorDeOpciones

---
+++ src/main/java/edu/fiuba/algo3/controlador/ManejadorDeOpciones.java
@@ -0,0 +1,21 @@
+package edu.fiuba.algo3.controlador;
+
+import edu.fiuba.algo3.modelo.Jugador;
+import edu.fiuba.algo3.modelo.Opcion;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class ManejadorDeOpciones implements EventHandler<ActionEvent> {
+    public Jugador jugador;
+    public Opcion opcion;
+
+    public ManejadorDeOpciones(Jugador jugador, Opcion opcion) {
+        this.jugador = jugador;
+        this.opcion = opcion;
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        opcion.agregarJugadorQueLaEligio(jugador);
+    }
+}
