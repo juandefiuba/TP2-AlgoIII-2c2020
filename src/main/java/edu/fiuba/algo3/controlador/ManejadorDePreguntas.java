6f70ba501a3a0a6c9632cf1db4fb5230bf6cf686
Micaela <micaelamjuarez@gmail.com>
2020-08-09 17:49:40 -0300

    Creo clase ManejadorDePreguntas

---
+++ src/main/java/edu/fiuba/algo3/controlador/ManejadorDePreguntas.java
@@ -0,0 +1,24 @@
+package edu.fiuba.algo3.controlador;
+
+import edu.fiuba.algo3.modelo.Jugador;
+import edu.fiuba.algo3.modelo.Preguntas.Pregunta;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+import java.util.LinkedList;
+
+public class ManejadorDePreguntas implements EventHandler<ActionEvent> {
+
+    private LinkedList<Jugador> jugadores;
+    private Pregunta pregunta;
+
+    public ManejadorDePreguntas(LinkedList<Jugador> jugadores, Pregunta pregunta) {
+        this.jugadores = jugadores;
+        this.pregunta = pregunta;
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        jugadores.forEach(jugador -> jugador.sumarPuntos(pregunta)); //Esto está chequeado?
+    }
+}
